7334e00ae45a1b4c69f95278c6c07c17d2dc1c66
wlroh <wlroh.paul@gmail.com>
2021-12-31 01:47:16 +0900

    commit

--- src/practice/puppetdraw/PuppetDrawSolution.java
+++ src/practice/puppetdraw/PuppetDrawSolution.java
@@ -1,27 +1,26 @@
 package practice.puppetdraw;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PuppetDrawSolution {
 
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
 
-        List<Integer> storedDolls = new ArrayList<>();
+        Stack<Integer> dollsStack = new Stack<>();
         for(int move : moves) {
             for(int i = 0 ; i < board.length ; i++) {
                 if (board[i][move - 1] > 0) {
-                    if(storedDolls.size() > 0) {
-                        Integer storedDoll = storedDolls.get(storedDolls.size() - 1);
-                        if(board[i][move - 1] == storedDoll) {
-                            storedDolls.remove(storedDolls.size() - 1);
-                            answer = answer + 2;
-                        } else {
-                            storedDolls.add(board[i][move - 1]);
-                        }
+                    if(dollsStack.isEmpty()) {
+                        dollsStack.push(board[i][move - 1]);
+                        board[i][move - 1] = 0;
+                        break;
+                    }
+                    if(board[i][move - 1] == dollsStack.peek()) {
+                        dollsStack.pop();
+                        answer += 2;
                     } else {
-                        storedDolls.add(board[i][move - 1]);
+                        dollsStack.push(board[i][move - 1]);
                     }
                     board[i][move - 1] = 0;
                     break;
